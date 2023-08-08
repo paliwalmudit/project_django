3bad783ec2de1a551b9868c8c8b114b59d4f861c
paliwalmudit <muditpaliwal2099@gmail.com>
2023-08-09 00:23:48 +0530

    small_changes

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,7 @@ app_name='main'
 
 urlpatterns = [
     path('',views.index , name='index'),
-    path('<str:name>/',views.inside,name='inside'),
+    path('inside/<str:name>/',views.inside,name='inside'),
     path('book/<str:name>',views.book,name='book'),
     path('service/<str:name>',views.service,name='service'),
     path('faq/<str:name>',views.faq,name='faq'),
